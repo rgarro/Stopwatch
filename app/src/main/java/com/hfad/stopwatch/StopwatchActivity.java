a14605ff21047c604b04b82fe0b11e249a06ccac
Rolando <rgarro@gmail.com>
2026-06-24 14:50:12 -0600

    handle posDelayed Time View

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -62,7 +62,7 @@ public class StopwatchActivity extends Activity {
                 int hours = seconds/3600;
                 int minutes = (seconds%3600)/60;
                 int secs = seconds%60;
-                String time = String.format(Locale.getDefault(),"%d:%02d",hours,minutes,secs);
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d",hours,minutes,secs);
                 timeView.setText(time);
                 if(running){
                     seconds ++;
